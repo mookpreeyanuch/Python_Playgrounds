2a97239001c912a95f244b88d1f58d2e6e3b165c
chavisa-mai <dechathipatmai@hotmail.com>
2022-04-09 16:55:48 +0700

    Update file

---
+++ def CalculatePi(roundVal):.py
@@ -0,0 +1,16 @@
+import math
+
+#attempt to personalize pi's decimal places
+def CalculatePi(roundPi):
+
+		somepi = round(User_num,roundPi);
+        User_num = int(input('Enter the number of digits you want after the decimal for Pi: \n'));
+		return User_num
+
+
+
+try:
+	print(f'Roundest Pi to your choice' {somepi}');
+except:
+	print("You did not enter an correctly");
+    
